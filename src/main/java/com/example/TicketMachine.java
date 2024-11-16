35046bb04fef2563f551bb84b6d9f982813ca74a
rohitsingh341 <rohitsingh341@gmail.com>
2024-11-16 19:43:02 +0000

    fix issue of record pattern matching

--- src/main/java/com/example/TicketMachine.java
+++ src/main/java/com/example/TicketMachine.java
@@ -3,6 +3,13 @@ package com.example;
 public class TicketMachine {
 
     public String getDetails(Ticket ticket) {
+
+        if (ticket instanceof AdultTicket(var price)) {
+            return "Adult ticket has price " + price;
+        }
+        else if (ticket instanceof ChildTicket(var price)){
+            return "Child ticket has price " + price;
+        }
         return "Unknown ticket type";
     }
 
